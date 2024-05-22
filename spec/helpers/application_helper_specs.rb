7329c2f26268ba87ac83d656f9008f2acb734779
VisheshP <vishesh.purohit19@gmail.com>
2024-05-22 23:21:37 +0530

    added string validation spec

--- spec/helpers/application_helper_specs.rb
+++ spec/helpers/application_helper_specs.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe ApplicationHelper, type: :helper do
-    
+
+    describe '#string_calculator' do
+        context 'validates string' do
+            it 'returns 0 if string is empty' do 
+                expect(string_calculator('')).to eq 0
+            end
+
+            it 'raises exception if string have is negative' do
+               expect{string_calculator('-1,2')}.to raise_error(RuntimeError)
+            end
+        end 
+    end
+
 end
