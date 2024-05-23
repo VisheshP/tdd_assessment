547c6a1ab22776ae8e3cbf9899906daa77f5f4a1
VisheshP <vishesh.purohit19@gmail.com>
2024-05-23 18:05:37 +0530

    code refactor

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,19 +1,16 @@
 module ApplicationHelper
     def string_calculator(input)
         return 0 if input.blank?
+        sum = 0
+        negative_num = []
+        delimiter = ','
 
-        if input.include?('//')
-            delimiter = input[2] 
-        else
-            delimiter = ','
-        end
-
+        delimiter = input[2] if input.include?('//')
+        
         input = input[5..] if !delimiter.include?(',')
 
         input = input.gsub('\n', ',').split(delimiter)
 
-        sum = 0
-        negative_num = []
         input.each do |num|
             int_num = num.to_i
             if int_num != 0
