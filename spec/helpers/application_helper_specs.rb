8bde91e0c4e5b502433a13f34001f3b8eea66442
VisheshP <vishesh.purohit19@gmail.com>
2024-05-23 17:39:17 +0530

    more spec added for delimiter and string validator

--- spec/helpers/application_helper_specs.rb
+++ spec/helpers/application_helper_specs.rb
@@ -8,10 +8,31 @@ RSpec.describe ApplicationHelper, type: :helper do
                 expect(string_calculator('')).to eq 0
             end
 
-            it 'raises exception if string have is negative' do
-               expect{string_calculator('-1,2')}.to raise_error(RuntimeError)
+            context 'raises RuntimeError' do
+                it 'string has negative number' do
+                    expect{string_calculator('-1,2')}.to raise_error(RuntimeError)
+                end
+
+                it 'string has negative number with message with all negative numbers' do
+                    expect{string_calculator('-1,2')}.to raise_error(RuntimeError, 'negative number not allowed [-1]')
+                end
+            end
+
+            it 'allows \n next line char in the sting' do
+                expect(string_calculator('1\n2,3')).to eq 6
             end
         end 
+
+        context 'allows to change delimiter with // as starting string and \n before number' do
+            it 'returns sum of integer seperated by ;' do
+                expect(string_calculator('//;\n1;2;3')).to eq 6
+            end
+
+            it 'returns sum of integer seperated by :' do
+                expect(string_calculator('//:\n3:5:9')).to eq 17
+            end
+        end
+
     end
 
 end
