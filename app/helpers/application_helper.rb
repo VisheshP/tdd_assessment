65938800d9f2972265e90887d1457b61c6e007ac
VisheshP <vishesh.purohit19@gmail.com>
2024-05-23 17:48:35 +0530

    loop to add nums and raise error

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,21 @@
 module ApplicationHelper
     def string_calculator(input)
         return 0 if input.blank?
+
+        
+        str_arr = input.split(',')
+        sum = 0
+        negative_num = []
+        str_arr.each do |num|
+            int_num = num.to_i
+            if int_num != 0
+                negative_num << int_num if int_num < 0
+                sum += int_num 
+            end
+        end
+
+        raise "negative number not allowed #{negative_num}" if !negative_num.blank?
+        
+        return sum
     end
 end
