b6a0407b1b05e63de2659f3601922d3d20504fd6
VisheshP <vishesh.purohit19@gmail.com>
2024-05-23 18:22:02 +0530

    code refactored

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,26 +1,20 @@
 module ApplicationHelper
     def string_calculator(input)
         return 0 if input.blank?
-        sum = 0
+
         negative_num = []
         delimiter = ','
 
-        delimiter = input[2] if input.include?('//')
-        
-        input = input[5..] if !delimiter.include?(',')
+        delimiter = input[2] if input.starts_with?('//')
 
+        input = input[5..] if !delimiter.include?(',')
         input = input.gsub('\n', ',').split(delimiter)
 
-        input.each do |num|
-            int_num = num.to_i
-            if int_num != 0
-                negative_num << int_num if int_num < 0
-                sum += int_num 
-            end
-        end
+        input = input.map(&:to_i)
+        negative_num = input.select { |n| n < 0 }
 
-        raise "negative number not allowed #{negative_num}" if !negative_num.blank?
+        raise "negative number not allowed #{negative_num}" if negative_num.any?
         
-        return sum
+        return input.sum
     end
 end
